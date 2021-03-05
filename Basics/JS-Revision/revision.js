9e16cda59ac3b496f1a08a23b2d370f04fec9c8c
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-03-05 18:42:41 +0530

    Add files via upload

---
+++ Basics/JS-Revision/revision.js
@@ -0,0 +1,19 @@
+alert("okayy world");
+//variables:
+//declaration
+var a;
+//initialization
+a="";
+//assigning value
+a="shibua";
+//display
+console.log(a);
+//constants:
+const pi=3.14;
+console.log(pi);
+//variable string + integer
+var message="";
+message="COUNTER BEFORE:"+20;
+console.log(message);
+//access the variable
+document.write(message);
