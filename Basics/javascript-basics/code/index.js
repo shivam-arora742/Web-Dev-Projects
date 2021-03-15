11cf46c4abad50186586dbcc7f54436fab7a93c7
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-03-15 22:07:24 +0530

    Add files via upload

---
+++ Basics/javascript-basics/code/index.js
@@ -0,0 +1,6 @@
+        // console API
+        console.log("Hello World!!!");
+        // alert("Hello World!!!");
+        document.write("HEllo WOrld");
+        console.warn("Helo Warning");
+        console.error("error here");
